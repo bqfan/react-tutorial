49c00afcb6f60678eee3a15c020da75a7fb7b650
Baiquan Fan <baiquan.fan@gmail.com>
2024-06-12 10:16:54 +0300

    Conditional Rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,103 @@
 import { UserDetails } from "./components/UserDetails";
 
 function App() {
-    const mockUsers = [
-        {
-            id: 1,
-            username: "user1",
-            email: "user1@example.com"
-        },
-        {
-            id: 2,
-            username: "user2",
-            email: "user2@example.com"
-        },
-        {
-            id: 3,
-            username: "user3",
-            email: "user3@example.com"
-        },
-        {
-            id: 4,
-            username: "user4",
-            email: "user4@example.com"
-        },
-    ];
+    // NOT_VERIFIED | VERIFIED | ACCOUNT_DISABLED'
+    const USER_STATUS = "VERIFIED";
 
-    return(
-        <div>
-            {mockUsers.map((user) => {
-                return <UserDetails user={user} />;
-            })}
-        </div>
-    );
+    switch (USER_STATUS) {
+        case "NOT_VERIFIED":
+            return (
+                <div>
+                    <span>
+                        You are not verified. Please verify your Email or Mobile Number.
+                    </span>
+                </div>
+            );
+        case "VERIFIED":
+            return (
+                <div>
+                    <span>
+                        You are verified. Congrets! Click here to access to your dashboard.
+                    </span>
+                </div>
+            );   
+        case "ACCOUNT_DISABLED":
+            return (
+                <div>
+                    <span>
+                        Your account is disabled.
+                    </span>
+                </div>
+            );
+        default:
+            return (
+                <div>
+                    <span>
+                        Please contact system admin.
+                    </span>
+                </div>
+            );                    
+    }
+
+    // if (USER_STATUS === "NOT_VERIFIED") {
+    //     return (
+    //         <div>
+    //             <span>
+    //                 You are not verified. Please verify your Email or Mobile Number.
+    //             </span>
+    //         </div>
+    //     );
+    // } else if (USER_STATUS === "VERIFIED") {
+    //     return (
+    //         <div>
+    //             <span>
+    //                 You are verified. Congrets! Click here to access to your dashboard.
+    //             </span>
+    //         </div>
+    //     );        
+    // } else if (USER_STATUS === "ACCOUNT_DISABLED") {
+    //     return (
+    //         <div>
+    //             <span>
+    //                 Your account is disabled.
+    //             </span>
+    //         </div>
+    //     );            
+    // } else {
+    //     return (
+    //         <div>
+    //             <span>
+    //                 Please contact system admin.
+    //             </span>
+    //         </div>
+    //     );          
+    // }
+
+    // const isAuthenticated = true;
+
+    // return isAuthenticated ? (
+    //     <div>
+    //         <h1>Welcome, User!</h1>
+    //     </div>
+    // ) : (
+    //     <div>
+    //         <span>Youare not logged in!</span>
+    //     </div>        
+    // );
+
+    // if (isAuthenticated) {
+    //     return (
+    //         <div>
+    //             <h1>Welcome, User!</h1>
+    //         </div>
+    //     );
+    // }
+
+    // return (
+    //     <div>
+    //         <span>Youare not logged in!</span>
+    //     </div>
+    // );
 }
 
 export default App;
