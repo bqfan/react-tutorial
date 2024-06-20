b581da387d263664b04ad472775fde168e33e91f
Baiquan Fan <baiquan.fan@gmail.com>
2024-06-20 17:57:59 +0300

    getByText query.

--- src/__tests__/UsernameDisplay.test.jsx
+++ src/__tests__/UsernameDisplay.test.jsx
@@ -1,10 +1,15 @@
 import { describe, it, test, expect } from 'vitest';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { UsernameDisplay } from '../components/UsernameDisplay';
 
 describe('UsernameDisplay', () => {
     it('should render username', () => {
         const result = render(<UsernameDisplay username="User 1" />);
         expect(result.container).toMatchSnapshot();
+        expect(screen.getByText("User 1")).toBeInTheDocument;
+        expect(screen.getByText(/User/)).toBeInTheDocument;
+        
+        const elements = screen.getAllByText("User 1");
+        elements.forEach((element) => expect(element).toBeInTheDocument());
     })
 })
