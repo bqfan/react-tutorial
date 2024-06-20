d7eee95d69e8804e0bb31e9f69a9b50c8dde6adc
Baiquan Fan <baiquan.fan@gmail.com>
2024-06-20 16:08:55 +0300

    Snapshot Testing.

--- src/__tests__/UsernameDisplay.test.jsx
+++ src/__tests__/UsernameDisplay.test.jsx
@@ -1,9 +1,10 @@
-import { describe, it } from 'vitest';
+import { describe, it, test, expect } from 'vitest';
 import { render } from "@testing-library/react";
 import { UsernameDisplay } from '../components/UsernameDisplay';
 
 describe('UsernameDisplay', () => {
     it('should render username', () => {
-        const result = render(<UsernameDisplay />);
+        const result = render(<UsernameDisplay username="User 1" />);
+        expect(result.container).toMatchSnapshot();
     })
 })
